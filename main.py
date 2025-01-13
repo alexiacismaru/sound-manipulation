ea95c79e582bf36d5cf68e4333e36ca7e1961c50
Alexia Cismaru <Alexia.cismaru@flowfactor.be>
2025-01-13 20:05:09 +0200

    detect hand

---
+++ main.py
@@ -0,0 +1,178 @@
+import csv
+import copy 
+import itertools 
+from collections import deque
+
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+ 
+from model import KeyPointClassifier 
+
+def main():  
+    cap = cv.VideoCapture(0)
+    use_brect = True # control if the bounding rectangle is drawn around the hand
+
+    # Model load  
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(max_num_hands=1,) # initialize hand tracking
+
+    keypoint_classifier = KeyPointClassifier() 
+
+    # Read labels  
+    with open('model/keypoint_classifier/keypoint_classifier_label.csv', encoding='utf-8-sig') as f:
+        keypoint_classifier_labels = csv.reader(f)
+        keypoint_classifier_labels = [row[0] for row in keypoint_classifier_labels] 
+  
+    while True: 
+        # Process Key (ESC: end)  
+        key = cv.waitKey(10)
+        if key == 27:  # ESC
+            break 
+
+        # Camera capture  
+        ret, image = cap.read()
+        if not ret:
+            break
+        image = cv.flip(image, 1)  # Mirror display
+        debug_image = copy.deepcopy(image)
+
+        # Detection implementation  
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+
+        if results.multi_hand_landmarks is not None:
+            for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
+                                                  results.multi_handedness):
+                # Bounding box calculation
+                brect = calc_bounding_rect(debug_image, hand_landmarks)
+                # Landmark calculation
+                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+
+                # Conversion to relative coordinates / normalized coordinates
+                pre_processed_landmark_list = pre_process_landmark(
+                    landmark_list) 
+                
+                # Hand sign classification
+                hand_sign_id = keypoint_classifier(pre_processed_landmark_list) 
+
+                # Drawing part
+                debug_image = draw_bounding_rect(use_brect, debug_image, brect) 
+                debug_image = draw_info_text(
+                    debug_image,
+                    brect,
+                    handedness,
+                    keypoint_classifier_labels[hand_sign_id], 
+                ) 
+ 
+        debug_image = draw_info(debug_image)
+
+        # Screen reflection 
+        cv.imshow('Hand Gesture Recognition', debug_image)
+
+    cap.release()
+    cv.destroyAllWindows()
+
+def calc_bounding_rect(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_array = np.empty((0, 2), int)
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point = [np.array((landmark_x, landmark_y))]
+
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+
+    x, y, w, h = cv.boundingRect(landmark_array)
+
+    return [x, y, x + w, y + h]
+
+def calc_landmark_list(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+    landmark_point = []
+
+    # Keypoint
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1) 
+        landmark_point.append([landmark_x, landmark_y])
+    return landmark_point
+
+def pre_process_landmark(landmark_list):
+    temp_landmark_list = copy.deepcopy(landmark_list)
+
+    # Convert to relative coordinates
+    base_x, base_y = 0, 0
+    for index, landmark_point in enumerate(temp_landmark_list):
+        if index == 0:
+            base_x, base_y = landmark_point[0], landmark_point[1]
+
+        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+    # Convert to a one-dimensional list
+    temp_landmark_list = list(
+        itertools.chain.from_iterable(temp_landmark_list))
+
+    # Normalization
+    max_value = max(list(map(abs, temp_landmark_list)))
+
+    def normalize_(n):
+        return n / max_value
+
+    temp_landmark_list = list(map(normalize_, temp_landmark_list))
+    return temp_landmark_list
+
+
+def pre_process_point_history(image, point_history):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    temp_point_history = copy.deepcopy(point_history)
+
+    # Convert to relative coordinates
+    base_x, base_y = 0, 0
+    for index, point in enumerate(temp_point_history):
+        if index == 0:
+            base_x, base_y = point[0], point[1]
+
+        temp_point_history[index][0] = (temp_point_history[index][0] -
+                                        base_x) / image_width
+        temp_point_history[index][1] = (temp_point_history[index][1] -
+                                        base_y) / image_height
+
+    # Convert to a one-dimensional list
+    temp_point_history = list(
+        itertools.chain.from_iterable(temp_point_history))
+
+    return temp_point_history
+
+def draw_bounding_rect(use_brect, image, brect):
+    if use_brect:
+        # Outer rectangle
+        cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[3]),
+                     (0, 0, 0), 1)
+    return image
+
+
+def draw_info_text(image, brect, handedness, hand_sign_text):
+    cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
+                 (0, 0, 0), -1)
+
+    info_text = handedness.classification[0].label[0:]
+    if hand_sign_text != "":
+        info_text = info_text + ':' + hand_sign_text
+    cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
+               cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
+    return image  
+
+def draw_info(image):  
+    return image
+
+if __name__ == '__main__':
+    main()
--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-
 class KeyPointClassifier(object):
     def __init__(
         self,
@@ -24,11 +23,7 @@ class KeyPointClassifier(object):
             input_details_tensor_index,
             np.array([landmark_list], dtype=np.float32))
         self.interpreter.invoke()
-
         output_details_tensor_index = self.output_details[0]['index']
-
         result = self.interpreter.get_tensor(output_details_tensor_index)
-
         result_index = np.argmax(np.squeeze(result))
-
         return result_index
